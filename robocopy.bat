3828c93211b2a88093b6a02230f4ac11bd3df5ad
vrobion <114609389+vrobion@users.noreply.github.com>
2024-08-08 17:58:20 +0200

    Add files via upload

---
+++ robocopy.bat
@@ -0,0 +1,3 @@
+permet de copier le contenu du dossier "robocopysource" vers "robocopydesti" en gardant les droits et en supprimant les fichier supprimé de la source
+
+robocopy "C:\robocopysource" "C:\robocopydesti" /copyall /e /purge
